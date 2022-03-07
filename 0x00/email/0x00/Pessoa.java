63d427d7fa60071918f4b146a11471d77b2be3de
barbararibeiroo <barbararibeiro18@gmail.com>
2022-03-07 16:41:21 -0300

    0x00

---
+++ 0x00/email/0x00/Pessoa.java
@@ -0,0 +1,12 @@
+
+public class Pessoa {
+
+
+	public static boolean emailValid(String emal) {
+		if(emal.contains("@") && emal.length() <= 50 ) {
+			return true;
+		}
+		return false;
+		
+	}
+}
---
+++ 0x00/email/0x00/TesteEmail.java
@@ -0,0 +1,13 @@
+
+public class TesteEmail {
+	
+	public static boolean testar_email_com_arroba() {
+		return Pessoa.emailValid("email_teste@dominio.com.br");
+	
+	}
+
+	public static boolean testar_email_mais_50_caracteres() {
+		return Pessoa.emailValid("email_teste@dominio.com.br");
+	}
+
+}
