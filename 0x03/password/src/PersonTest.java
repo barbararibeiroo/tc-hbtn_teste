fed2f7291e901375d72e6a0f75c1891a0f9051b8
barbararibeiroo <barbararibeiro18@gmail.com>
2022-03-10 16:13:38 -0300

    0x03

--- 0x03/password/src/PersonTest.java
+++ 0x03/password/src/PersonTest.java
@@ -1,39 +1,57 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
-public class PersonTest {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    public class PersonTest {
         Person person;
 
-        @BeforeEach
-        public void setup() {
-            person = new Person("Paul", "McCartney", 2000, true, true, true);
-
-        }
-        @Test
-        public void show_full_name() {
-            assertEquals("Paul McCartney", person.fullName());
-
-        }
-        @Test
-        public void test_calculateYearlySalary() {
-            person.setSalary(1200);
-            assertEquals(person.calculateYearlySalary(), 14400);
-        }
-        @Test
-        public void person_is_MEI(){
-            person.setAnotherCompanyOwner(false);
-            person.setPensioner(false);
-            person.setPublicServer(false);
-            assertTrue(person.isMEI());
-        }
-        @Test
-        public void person_is_not_MEI(){
-            assertFalse(person.isMEI());
+        @BeforeAll
+        public void setup(){
+            person = new Person();
         }
 
+        @ParameterizedTest
+        @ValueSource( strings = {"PaulMcCartney2", "NeilArms2"})
+        void check_user_valid(String loginUsuario){
+            assertTrue(person.checkUser(loginUsuario));
+        }
 
+        @ParameterizedTest
+        @ValueSource(strings = {"Paul#McCartney", "Neil@Arms"})
+        void check_user_not_valid(String loginUsuario){
+            assertTrue(person.checkUser(loginUsuario));
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"123456789", "#$%1234"})
+        void does_not_have_letters(String senhaUsuario){
+            assertFalse(person.checkPassword(senhaUsuario));
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"Abcabcdefgh@", "#hbtn@%tc"})
+        void does_not_have_numbers(String senhaUsuario){
+            assertFalse(person.checkPassword(senhaUsuario));
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"Abc@123", "12$@hbt"})
+        void does_not_have_eight_chars(String senhaUsuario){
+            assertFalse(person.checkPassword(senhaUsuario));
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123"})
+        void check_password_valid(String senhaUsuario){
+            assertTrue(person.checkPassword(senhaUsuario));
+        }
 
     }
+
 
 
